38cfb5bee96988c96110fddee0870b61ca325364
OlegSam2 <doorsmania@mail.ru>
2022-08-10 18:54:53 +0300

    task11 was realized

--- task11/Program.cs
+++ task11/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 11. Напишите программу, которая выводит случайное трёхзначное число
+// и удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+int number = new Random().Next(100, 1000); 
+Console.WriteLine($"Случайное число = {number}");
+int digit1 = number / 100; // 1вые цифры числа
+int digit3 = number % 10;  // 3тьи цифры числа
+int res = digit1 * 10 + digit3; // 10 так как число будет двузначное
+
+Console.WriteLine($"Результат= {res}");
